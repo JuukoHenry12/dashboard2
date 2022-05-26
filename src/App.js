b397505d437d3855fc6f1d23022196159b84943c
juukohenry23 <juukohenry23@gmail.com>
2022-05-26 17:30:31 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 import {FiSettings} from "react-icons/fi"
 import Sidebar from './Components/Sidebar'
+import Navbar from './Components/Navbar'
+import {Ecommernce,Order,Kanban,Editor,Customers,Calendar,ColorPicker,
+Area,Bar,ColorMapping,Pyramid,Pie,Line,Stacked,Financial, Employee,ThemeSettings
+} from './pages'
 function App() {
   const activeMenu =true
   return (
@@ -20,15 +24,46 @@ function App() {
         </div>
         {
           activeMenu ? (
-            <div>
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
              <Sidebar/>
             </div>
           ):(
-            <div>
+            <div className="w-0 dark:bg-secondary-dark-bg">
               <Sidebar/>
             </div>
           )
         }
+      <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72':'flex-2'}`}>
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+             <Navbar/>
+          </div>
+      </div>
+      <div>
+        <Routes>
+       
+          {/* Dashboard */}
+          <Route path='/' element={<Ecommernce/>}/>
+          <Route path='/ecommerce' element={<Ecommernce/>}/>
+          <Route path='/order' element={<Order/>}/>
+          <Route path='/customers' element={<Customers/>}/>
+          <Route path='/editor' element={<Editor/>}/>
+          <Route path='/calendar' element={<Calendar/>}/>
+          <Route path='/employee' element={<Employee/>}/>
+          <Route path='/kanban' element={<Kanban/>}/>
+          <Route path='/financial' element={<Financial/>}/>
+
+          {/* Charts */}
+          <Route path='/line' element={<Line/>}/>
+          <Route path='/bar' element={<Bar/>}/>
+          <Route path='/area' element={<Area/>}/>
+          <Route path='/color-mapping' element={<ColorMapping/>}/>
+          <Route path='/pie' element={<Pie/>}/>
+          <Route path='/pyramid' element={<Pyramid/>}/>
+          <Route path='/stacked' element={<Stacked/>}/>
+          <Route path='/colorpicker' element={<ColorPicker/>}/>
+          
+        </Routes>
+      </div>
       </div>
       </BrowserRouter>
     </div>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,61 +1,60 @@
 import React from 'react'
-import {Link,NavLink} from 'react-router-dom'
-import {SiShopware} from 'react-icons/si'
-import {MdOutlineCancel} from 'react-icons/md'
+import { Link, NavLink } from 'react-router-dom'
+import { SiShopware } from 'react-icons/si'
+import { MdOutlineCancel } from 'react-icons/md'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import {links} from '../data/dummy'
-
-function Sidebar() {
-  const activeMenu =true ;
+import { links } from '../data/dummy'
+const Sidebar = () => {
+  const activeMenu =true;
+  const activeLink ='flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 '
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
   return (
-    <div className='ml-3 h-screen md:overflow-hidden overflow-auto
-    md:hover:overflow-auto pb-10
-    '>
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
      {
        activeMenu && (
          <>
-         <div className='flex justify-between items-center'>
-           <Link to="/" onClick={()=>{}} 
-            className="item-center gap-3 ml-3
-            mt-4 flex text-xl font-extrabold
-            tracking-tight dark:text-white
-            text-slate-900
+        <div className="flex justify-between items-center">
+           <Link to="/" onClick={()=>{}}
+            className="item-center gap-4 ml-3 mt-4 flex text-xl tracking-tight
+            dark:text-white text-slate-900
             "
            >
-           <SiShopware/><span>Shoppy</span>
+            <SiShopware/><span>Shoppy</span>
+          
            </Link>
-         </div>
-         <TooltipComponent content="Menu" position="BottomCenter">
-           <button type='button'
-            onClick={()=>{}}
-            className="text-xl rounded-full p-3 hover:bg-light-gray mt-4
-             block md:hidden
-            "
-           >
-             <MdOutlineCancel/>
-           </button>
-         </TooltipComponent >
-
-         <div className='mt-10'>
+           <TooltipComponent content="Menu" position="BottomCenter">
+             <button type="button"
+              onClick={()=>{}}
+              className="text-xl rounded-full p-3 hover:bg-light-gray
+              mt-4 block md:hidden">
+               <MdOutlineCancel/>
+             </button>
+           </TooltipComponent>
+        </div>
+        <div className="mt-10">
           {
-            links.map((item)=>
-             <div key={item.title}>
-               <p className="text-gray-400 m-3 mt-4 uppercase">
-                {item.title}
-               </p>
-               {
-                 item.links.map((link)=>(
-                   <NavLink
+            links.map((item)=>(
+              <div key={item.title}>
+                <p className='text-gray-400 m-3 mt-4 uppercase'>
+                  {item.title}
+                </p>
+                {
+                  item.links.map((link)=>(
+                    <NavLink
                     to={`/${link.name}`}
-                   />
-                 ))
-               }
-              </div>
-            )
-          }
-         </div>
+                    key={link.name}
+                    onClick={()=>{}}
+                    className={({isActive})=>{}}
+                    >
 
-         </>
+                    </NavLink>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
+        </>
        )
      }
     </div>
